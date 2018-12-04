2c5764dff9716bc57797f401651cd00398a8e5d3
Jaime Loeuf <jaimeloeuf@gmail.com>
2018-12-04 23:31:53 +0800

    Changes: 1.	Commented out all the server creation code in index.js. Created a new 	version by stripping out all the event listeners. Added a process.On 	eventlistener for the beforeExit event to try to see what happens when 	the code exits.
    	Below is the Siege command used to test as 20 users with 10 req each.
    	artillery quick --count 10 -n 20 http://localhost:3000/sample
    
    	Update: Nothing shows up on the log. It still exits halfway seems like It
    	skipped async console.log method. So perhaps it was a force stop or out
    	of ram?

--- index.js
+++ index.js
@@ -15,23 +15,31 @@ const v8 = require('v8'); // Used to see memory usage
 // Convert avail memory from bytyes to Gigabytes and round to 2 d.p.
 log(`The total memory for this process is: ${(v8.getHeapStatistics().total_available_size / 1024 / 1024 / 1024).toFixed(2)}GB`);
 
+// http.createServer(unifiedServer)
+// 	.listen(env.port, (err) => {
+// 		log(`Node running in the '${env.envName}' mode/environment`)
+// 		log(((err) ? 'Error, server cannot listen on port: ' : 'Server listening on port: ') + env.port);
+// 		if (err)
+// 			process.exit();
+// 	})
+// 	.on('error', (err) => {
+// 		log(err)
+// 		process.exit();
+// 	})
+// 	.on('clientError', (err, socket) => {
+// 		if (err) log(err);
+// 		socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+// 	})
+// 	.on('close', () => {
+// 		// Last actions before the server closes. Perhaps send a close success/error signal back to process caller?
+// 		log('Server closed');
+// 		process.exit();
+// 	});
+
 http.createServer(unifiedServer)
 	.listen(env.port, (err) => {
 		log(`Node running in the '${env.envName}' mode/environment`)
 		log(((err) ? 'Error, server cannot listen on port: ' : 'Server listening on port: ') + env.port);
-		if (err)
-			process.exit();
 	})
-	.on('error', (err) => {
-		log(err)
-		process.exit();
-	})
-	.on('clientError', (err, socket) => {
-		if (err) log(err);
-		socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
-	})
-	.on('close', () => {
-		// Last actions before the server closes. Perhaps send a close success/error signal back to process caller?
-		log('Server closed');
-		process.exit();
-	});
+
+process.on('beforeExit', (code) => log(`The exit code caught is ${code}`));
