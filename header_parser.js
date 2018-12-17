07967022aa42dbc3add02cab07545585d27ae50f
Jaime Loeuf <jaimeloeuf@gmail.com>
2018-12-18 01:04:13 +0800

    Changes: 1.	Added some code that deals with JWT into the header_parser module, 	to temporarily test and use it. 	Might create a seperate module to deal with JWT soon once I get the 	DataBase part done.
    2.	For some reasons, I did not include the delete parser module action
    	into the previous commit where I renamed the parser module to body_parser
    	for better naming clarity. So I am commiting this action now.

--- header_parser.js
+++ header_parser.js
@@ -12,4 +12,29 @@ function he(req) {
 	}
 }
 
+// Deal with the JWT Token here?
+// ctx.token = token.split(' ')[1]
+function verifyJWT(ctx, next) {
+	// Get auth in header
+	token = ctx.token;
+	if (token) {
+
+	}
+	else
+		finalHandler(403);
+}
+
+jwt.verify(token, secretKey, (err, AuthData) => {
+	if (err)
+		finalHandler(403);
+
+});
+
+
+let r_val;
+function wrapper(ctx, fn) {
+	r_val = fn(ctx);
+	return (r_val === undefined) ? ctx : r_val;
+}
+
 // Should I really seperate out this into a seperate module
