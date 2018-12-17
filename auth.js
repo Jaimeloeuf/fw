fd213e7b45595c17ab4e6b7ef96b33619ad0485e
Jaime Loeuf <jaimeloeuf@gmail.com>
2018-12-18 01:42:23 +0800

    Changes: 1.	Refactored out the JWT code from handlers module into auth module. 	To work on it and fix the problem where I need to return the Promise 	that was returned from auth module in order for the server module's 	Promise thenable chaining to work.

--- auth.js
+++ auth.js
@@ -27,3 +27,81 @@ const authenticate = (user) => {
 		
 	});
 };
+
+
+// Dependencies
+const jwt = require('jsonwebtoken');
+
+// Mock user
+const user = {
+	id: 1,
+	username: 'brad',
+	email: 'brad@gmail.com'
+}
+
+const log = (dat) => console.log(dat);
+
+
+
+module.exports.createToken = (ctx) => {
+	// Do the db shit then get back the user to create token. To refactor the create token code too.
+	return new Promise((resolve, reject) => {
+		jwt.sign({ user }, 'secretkey', { expiresIn: '1000s' }, (err, token) => {
+			if (err)
+				reject(err);
+			// ctx.res.write(JSON.stringify(token));
+			// ctx.res.setHeader({Auth: token});
+			ctx.res_body.token = token; // Write token to response body to be sent back in the finalHandler
+			ctx.res_headers['Set-Cookie'] = token;
+			resolve(ctx);
+		});
+	});
+}
+
+module.exports.verify = (ctx) => {
+	getToken(ctx); // Sync call to get the token out of headers
+	return new Promise((resolve, reject) => {
+		jwt.verify(ctx.token, 'secretkey', (err, authData) => {
+			if (err) {
+				ctx.statusCode = 403; // Forbidden
+				ctx.res_body.error = 'Forbidden, invalid auth';
+				// reject(err); // Don't reject as it is not a code/logic error, but a user error
+				resolve(ctx);
+			}
+			else {
+				// Send back the details the client requested for
+				ctx.res_body.message = 'Post created...';
+				ctx.res_body.authData = authData; // Probs not going to send this back to the user
+				resolve(ctx);
+			}
+		});
+
+	})
+}
+
+
+// FORMAT OF TOKEN
+// Authorization: Bearer <access_token>
+var header;
+// function getToken(ctx) {
+// 	header = ctx.headers['authorization']; // Get auth header value
+// 	if (header === 'undefined')
+// 		log('hi')
+// 	if (typeof header === 'undefined') // Check if bearer is undefined
+// 		ctx.statusCode = 403; // Forbidden
+// 	const bearerToken = bearerHeader.split(' ')[1]; // Split at the space and Get token from array
+// 	ctx.token = bearerToken; // Set the token
+// }
+
+function getToken(ctx) {
+	return new Promise((resolve, reject) => {
+		header = ctx.headers['authorization']; // Get auth header value
+		if (header === 'undefined')
+			reject('No auth header found');
+		if (typeof header === 'undefined') // Check if bearer is undefined
+			ctx.statusCode = 403; // Forbidden
+		const bearerToken = header.split(' ')[1]; // Split at the space and Get token from array
+		ctx.token = bearerToken; // Set the token
+		resolve(ctx);
+	});
+}
--- handlers.js
+++ handlers.js
@@ -7,7 +7,7 @@
 */
 
 // Dependencies
-const jwt = require('jsonwebtoken');
+const { createToken, verify } = require('./auth');
 
 const handler = {};
 
@@ -26,59 +26,14 @@ var _user = {
 	}
 };
 
-// Mock user
-const user = {
-	id: 1,
-	username: 'brad',
-	email: 'brad@gmail.com'
-}
-
-const log = (dat) => console.log(dat);
-
 handler.login = (ctx) => {
-	// Do the db shit then get back the user to create token. To refactor the create token code too.
-	return new Promise((resolve, reject) => {
-		jwt.sign({ user }, 'secretkey', { expiresIn: '1000s' }, (err, token) => {
-			if (err)
-				reject(err);
-			// ctx.res.write(JSON.stringify(token));
-			// ctx.res.setHeader({Auth: token});
-			ctx.res_body.token = token; // Write token to response body to be sent back in the finalHandler
-			resolve(ctx);
-		});
-	});
+	return createToken(ctx);
 };
 
 handler.user = (ctx) => {
-	getToken(ctx); // Sync call to get the token out of headers
-	return new Promise((resolve, reject) => {
-		jwt.verify(ctx.token, 'secretkey', (err, authData) => {
-			if (err) {
-				ctx.statusCode = 403; // Forbidden
-				ctx.res_body.error = 'Forbidden, invalid auth';
-				// reject(err); // Don't reject as it is not a code/logic error, but a user error
-				resolve(ctx);
-			}
-			else {
-				// Send back the details the client requested for
-				ctx.res_body.message = 'Post created...';
-				ctx.res_body.authData = authData; // Probs not going to send this back to the user
-				resolve(ctx);
-			}
-		});
-
-	})
+	return verify(ctx);
 }
 
-// FORMAT OF TOKEN
-// Authorization: Bearer <access_token>
-function getToken(ctx) {
-	const bearerHeader = ctx.headers['authorization']; // Get auth header value
-	if (typeof bearerHeader === 'undefined') // Check if bearer is undefined
-		ctx.res.statusCode = 403; // Forbidden
-	const bearerToken = bearerHeader.split(' ')[1]; // Split at the space and Get token from array
-	ctx.token = bearerToken; // Set the token
-}
 
 // Login handler
 // handler.login = (ctx) => {
