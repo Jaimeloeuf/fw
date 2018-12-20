422db374732aa925a4cc7bcb6b580ee3a71bad2d
Jaime Loeuf <jaimeloeuf@gmail.com>
2018-12-20 21:35:37 +0800

    Changes: 1.	In the req_payload module, I refactored the code that checks for the size 	of the incoming request's message into a seperate function that is defined 	in the same scope.

--- req_payload.js
+++ req_payload.js
@@ -23,13 +23,7 @@ module.exports = (ctx) => {
 		buffer = ''; // Reset/clear buffer
 		ctx.req
 			// .on('data', data => buffer += decoder.write(data))
-			.on('data', data => {
-				buffer += decoder.write(data)
-				if (buffer.length > max_size) {
-					ctx.statusCode = 413;
-					ctx.newError('Post entity too big')
-				}
-			})
+			.on('data', newData(data))
 			.on('error', error => reject(error))
 			.on('end', () => {
 				// If no payload, then 'end' event is fired immediately
@@ -39,5 +33,21 @@ module.exports = (ctx) => {
 					ctx.req_body = undefined; // Make sure that req_body is undefined if current buffer is empty
 				resolve(ctx); // Resolve with 'ctx' for the next function in 'then' chaining
 			});
+
+		/*	Out of the 2 function declaration methods, I'm using the arrow function, because if I'm not
+			wrong, the arrow function will inherit and share the same lexical scope as the parent scope.
+			Which will allow it to access 'buffer' directly? Not exactly clear on this yet.  */
+		// function newData(data) {
+		const newData = (data) => {
+			buffer += decoder.write(data)
+			if (buffer.length > max_size) {
+				ctx.statusCode = 413;
+				ctx.newError('Post entity too big');
+				
+				// Decide if I should resolve or reject this error
+				resolve(ctx); // Resolve and let finalHandler deal with the error
+				reject(ctx); // Reject and ask finalHandler send back a 500
+			}
+		}
 	});
 }
