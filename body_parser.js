6bafe95f2b0252e3d1232393adbaa0f9a1d1532c
Jaime Loeuf <jaimeloeuf@gmail.com>
2018-12-27 23:48:20 +0800

    Changes: 	Fixed a few bugs with body_parser module and added new check for plain 	text message bodies.

--- body_parser.js
+++ body_parser.js
@@ -10,12 +10,11 @@ module.exports = (ctx) => {
 	return new Promise((resolve, reject) => {
 		// Parse payload if any
 		if (ctx.req_body) {
-			// Parse buffer if JSON format is specified
-			if (ctx.checkContentType('application/json')) {
+			if (ctx.checkContentType('application/json')) { // Check for JSON format
 				try { ctx.req_body = JSON.parse(ctx.req_body); }
 				catch (error) { return reject(error); }
 				// ^Why do I need to return a reject here? Oh ya, return stops it from continuing? THast so stupid
-				
+
 				// parseJSON(ctx.req_body)
 				// 	.then((data) => ctx.req_body = data)
 				// 	.catch((err) => reject(err))
@@ -24,12 +23,13 @@ module.exports = (ctx) => {
 
 				// Use the generator waterfall thing?
 			}
-			else if (ctx.checkContentType('application/x-www-form-urlencoded'))
+			else if (ctx.checkContentType('application/x-www-form-urlencoded')) // Check for URL-encoded Key/Value pairs
 				ctx.req_body = urlencodedParser(ctx.req_body); // Does this need try/catch too? Might throw error?
+			else if (ctx.checkContentType('text/plain')); // Do nothing if it is plain text
 			else
 				return reject(`ERROR: Unknown content-type for payload received: ${ctx.contentType}`);
 		}
-		// resolve called after if block so next function in Promise chain always called even if payload is empty
-		resolve(ctx); // Resolve with 'ctx' for the next function in 'then' chaining
+		// resolve called after if block Promise is always resolved even if payload is empty.
+		resolve(); // Do I need to return the resolve now? Since this is the last line of code in this block
 	});
 };
