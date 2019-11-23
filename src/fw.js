de0145d3e91cda6137eca1d7cfa8e73d12c20868
Jaime <jaimeloeuf@gmail.com>
2019-11-23 20:39:13 +0800

    Remove unused code.

--- src/fw.js
+++ src/fw.js
@@ -14,13 +14,6 @@ const router = require('./router');
 // Function for setting a new route and its handler into the router HashMap
 const setHandler = (method, route, handler) => router[method][route] = handler;
 
-// module.exports.route = setHandler; // The generic route and handler setter.
-// Below are the function wrappers for setHandler function with the specified verb.
-// module.exports.post = (route, handler) => setHandler('POST', route, handler);
-// module.exports.get = (route, handler) => setHandler('GET', route, handler);
-// module.exports.put = (route, handler) => setHandler('PUT', route, handler);
-// module.exports.del = (route, handler) => setHandler('DEL', route, handler);
-
 module.exports = {
 	route: setHandler, // The generic route and handler setter.
 	post: (route, handler) => setHandler('POST', route, handler),
